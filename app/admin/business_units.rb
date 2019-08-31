444e9322628a69308224e892cd9171b2381b6285
Raghu Betina <raghu@firstdraft.com>
2019-08-31 14:42:16 +0000

    Whitelist columns in BusinessUnit for ActiveAdmin

--- app/admin/business_units.rb
+++ app/admin/business_units.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register BusinessUnit do
+
+ permit_params :business_unit, :director_id, :operating_unit_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
