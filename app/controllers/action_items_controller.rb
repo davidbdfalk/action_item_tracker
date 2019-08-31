f2428139e61872ba1111f39e46476e2adda3d634
Raghu Betina <raghu@firstdraft.com>
2019-08-31 14:42:37 +0000

    Only action_item user can modify action_item

--- app/controllers/action_items_controller.rb
+++ app/controllers/action_items_controller.rb
@@ -1,4 +1,14 @@
 class ActionItemsController < ApplicationController
+  before_action :current_user_must_be_action_item_sender, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_action_item_sender
+    action_item = ActionItem.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == action_item.sender
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @action_items = ActionItem.all
 
